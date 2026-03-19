a196f7391289d58a9f88ba6d66cfe76bf02017df
Kavindi487 <kavindividusari@gmail.com>
2026-03-19 22:06:42 +0530

    .....

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,15 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent, NavbarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('frontend');
+  title = 'TaskManager';
 }
---
+++ frontend/src/app/pages/tasks-page/tasks-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-tasks-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tasks-page.component.html',
+  styleUrl: './tasks-page.component.scss'
+})
+export class TasksPageComponent {
+
+}
